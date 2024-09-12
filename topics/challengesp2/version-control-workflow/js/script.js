e47cbc762c2b794dead7c68b80c2c6ba47e8bad6
mcbeaniee <jackalope128@gmail.com>
2024-09-12 14:10:35 -0400

    added challengesp2 project and moved to topics folder

---
+++ topics/challengesp2/version-control-workflow/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Jack's version control project
+ * Jack McDonald
+ * 
+ * 
+ * Testing out version control and stuff in p5.js
+ */
+
+"use strict";
+
+/**
+ * Sets up rules before running draw function.
+*/
+function setup() {
+
+}
+
+
+/**
+ * draws pixels on the screen according to the code in the function.
+*/
+function draw() {
+
+}
