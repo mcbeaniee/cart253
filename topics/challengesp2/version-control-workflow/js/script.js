9de76da0b33d3e9144e6e6c9a49e80cc238cc49a
mcbeaniee <jackalope128@gmail.com>
2024-09-18 18:08:48 -0400

    added pre-reflection and tweaked alt text in challenges project

--- topics/challengesp2/version-control-workflow/js/script.js
+++ topics/challengesp2/version-control-workflow/js/script.js
@@ -12,13 +12,46 @@
  * Sets up rules before running draw function.
 */
 function setup() {
-
+    createCanvas(500,500);
 }
 
 
 /**
  * draws pixels on the screen according to the code in the function.
-*/
-function draw() {
+ * #1 is the italian flag, draws background and draws flag function.
+ * #2 is the black chain and yellow background
 
+function draw() {
+    //draws blue background
+    background("#5873e0");
+    //flag
+    drawFlag();
+}
+
+function drawFlag() {
+    push(); 
+    noStroke();
+    fill("#2e7d38"); 
+    rect(60,125,125,[250]);
+    fill("#ff0000")
+    rect(310,125,125,[250]);
+    fill("#ffffff")
+    rect(185,125,125,[250]);
+    pop();
+}
+*/
+function draw(){
+    //draws yellow background
+    background("#ffff00");
+    //ellipse chains
+    drawChain();
+}
+
+function drawChain(){
+    push();
+    strokeWeight(10);
+    stroke("#000000");
+    fill("#ffff00");
+    ellipse(100,100,100,[300]);
+    pop();
 }
