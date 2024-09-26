ec5f259c27ad5960743dc4a6459ee2303cee74bb
mcbeaniee <jackalope128@gmail.com>
2024-09-26 12:30:26 -0400

    updated sript to work with mrfurious challenge

--- topics/thinking-like-a-computer-with-variables/js/script.js
+++ topics/thinking-like-a-computer-with-variables/js/script.js
@@ -1,56 +1,59 @@
 /**
- * Thinking like a computer with variables
+ * Mr. Furious
  * Pippin Barr
- * 
- * A shape moves slowly across the screen
+ *
+ * A guy who becomes visibly furious!
  */
 
 "use strict";
 
-// We set the frame-rate with this
-const rate = 2;
-
-// The shade of the sky (starts out at night)
-let skyShade = 0;
-
-// Our turtle that will move across the canvas
-let turtle = {
-    // Position, will set in setup
-    x: undefined,
-    y: undefined,
-    // How big is the turtle circle
-    size: 50,
-    // How many pixels it moves per frame
-    speed: 10
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225
+  }
 };
 
-/**
- * Create the canvas, set up the turtle's position
-*/
-function setup() {
-    createCanvas(600, 600);
-
-    // Set the framerate
-    frameRate(rate);
-
-    // Position the turtle at the bottom left
-    turtle.x = 0;
-    turtle.y = height;
+let randomShake = {
+    x1: 199,
+    x2: 201
 }
 
 /**
- * Move and draw the turtle
-*/
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
+}
+
+/**
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
-    // Make it lighter
-    skyShade = skyShade + 10;
+  background(160, 180, 200);
 
-    // The sky
-    background(skyShade);
-
-    // Move the turtle
-    turtle.x += turtle.speed;
-
-    // Draw the turtle (it walks with its head inside its shell okay?)
-    ellipse(turtle.x, turtle.y, turtle.size);
+  // Mr Furious gets redder (constrained)
+  mrFurious.fill.g -=1;
+  //mrFurious.fill.g = constrain[mrFurious.fill.g,0,255];
+  mrFurious.fill.b -=1;
+  //mrFurious.fill.b = constrain[mrFurious.fill.b,0,255];
+  
+  randomShake.x1 +=1;
+  randomShake.x1 = constrain[randomShake.x1,199,190]
+  randomShake.x2 +=1;
+  randomShake.x2 = constrain[randomShake.x2,201,210]
+  mrFurious.x = random(randomShake.x1,randomShake.x2)
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
 }
