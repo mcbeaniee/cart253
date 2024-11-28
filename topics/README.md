c43be70d71d91283b85428603a724dcf627cd4a8
mcbeaniee <jackalope128@gmail.com>
2024-11-28 15:01:58 -0500

    Finished fishing minigame pseudocode and implemented rough structure into script.js framework

---
+++ topics/README.md
@@ -0,0 +1,2 @@
+# cart253
+Coursework repository for Pippin Barr's CART 253
--- topics/variation-jam-jack/js/script.js
+++ topics/variation-jam-jack/js/script.js
@@ -7,17 +7,162 @@
 
 "use strict";
 
-/**
- * sets up scene
-*/
+//FISH minigame, base for variations
+
+function preload(){
+//img()
+}
+let scarcity; //(determines how many fish will be in the water, increases with overfishing, resets when fish are sold)
+let gameState = 'fishin';
+
+//rod object, contains hook, line and sink- i mean shaft
+const rod = {
+hook: {
+    x = //controlled by arrow keys
+    y = //current reel level
+    size = //size of asset
+    hooked = false
+},
+shaft:   {
+    x1 = //player sprite x position
+    x2 = //hook.y, to be later constrained for realism
+    y1 = //player sprite y position
+    y2 = //constant position determined by whether or not a fish is being reeled in or not
+    fishHooked = false //determines y position
+},
+line:    {
+    //i dont know how to make the line curve/ swing to location rather than a straight line but i know theres a way to do it
+    x1 = hook.X,
+    x2 = rod.x2,
+    y1 = hook.y,
+    y2 = rod.y2
+}
+}
+
+//fish object, to be pushed into an array
+class fish{
+constructor (speed,size,start){
+    x = 0,//moves across screen from initated position
+    y = 0,//random from surface to floor of screen 
+    size = 0,//random 
+    value = 0,//ranges depending on different size caps (ex: 1-5 = $10 6-10 = $15 etc)
+    speed = 0,//random but pretty slow
+    start = 0,//randomizes right moving or left moving
+    radius = 0//invisible rectangle around fish that is its detection radius, = size * by random number
+}
+}
+
+//fishies array. yes, i know the plural of fish is fish
+let fishies = []
+
+//setup, create canvas, reset/initialize variables
 function setup() {
-
+timetofish();
 }
 
-
-/**
- * runs functions and draws on the canvas
-*/
+//draws assets and objects, as well as pushes array
 function draw() {
+switch(gameState){
+    /*shop is a non-essential. 
+    case 'shop':
+        shop assets 
+        buyUpgrades()
+        break;
+    */
+    case 'cash':
+        //moves back to fishin when "next day" is pressed"
+        //cash assets
+        cashFish();
+        break;
+    case 'fishin':
+        img(assets) //backgrounds, player location, and sea floor. as well as moving water sprite.
+        moveFish();
+        resetFish();
+        pushFish();
+        drawFish();
+        drawRod();
+        catchFish();
+    break;
+}
+}
+
+//add fish speed to fish x. functionally identical to //moveFly()
+function moveFish() {
+    move fish
+    if (fish x is past the its opposite side of the canvas){
+        move the fish back to the left
+        give the fish a random y position
+    } 
+}
+
+//pushes new fish to array, up to a limit of the current scarcity level
+function pushFish() {
+    for (scarcity range) {
+        fishies.push(randomize size, speed and start location)
+    }
+}
+
+//resets fish location after being reeled and spawns a new one, so long as there is enough time and scarcity space
+function resetFish() {
 
 }
+
+//draws fish, randomized textures.
+function drawFish() {
+    if (random number) = 1
+    img(asset,size)
+    else if = 2
+    img(asset,size)
+    else if = 3 etc etc
+}
+
+/*
+code for moving and controlling rod.hook and rod.line parameters
+*/
+function drawRod() {
+    moves hook and end of line X based on arrow keys. left/right
+    moves hook and end of line Y down when button F is pressed (at  a constant rate) press F again to reeling down. plays unspool sound loop
+    moves hook and end of line Y up at a rate determined by current reel in speed, when button R is held: when no fish is on th line and MASHED: when rod.hook.hooked = true. (reel) plays reeling in sound loop
+    
+    slowly moves rod down when fish is on the line (rod.hook.hooked = true) at a rate proportional to fish.size and fish.speed. 
+    when fish is hooked, also change rod.shaft.fishHooked to secondary postion and make it shake a little bit.
+}
+
+//checks if hook overlaps fish.radius, and make random checks until fish bites.
+function catchFish() { 
+    when fish bites: rod.hook.hooked = true
+    if fish reaches x pixels above level fish was hooked, 
+        fish will be caught. 
+    else fish reaches location x pixels lower than level fish was hooked, 
+        rod breaks and fully reels up. fish is lost.
+}   
+
+//plays day/night cycle and switches state to fish cashin when time runs out. 
+function timeToCash() { 
+
+}
+
+//sells fish and moves on to next day, resetting gameState
+cashFish() {
+    if E key pressed to sell all fish, 
+        sells fish at current market rate.
+}
+
+
+/** for stats and upgrades function, work on after initial project is working and assets are solidified.
+buyUpgrades()
+shows upgrades 1 2 and 3, randomized from upgrades.json
+can be basic to premium in rarity and include reel strength, reel speed and 
+/fisherman stats
+let stats = {
+all values start at 1, as they are multipliers 
+speed = speed at which reel in and unspool happens
+strength = strength of mash when fish is hooked. like webfishing
+value = multiplier or flat increase? to fish value
+bait = makes fish more likely to bite the higher the stat is
+}
+*/
+
+
+
+
